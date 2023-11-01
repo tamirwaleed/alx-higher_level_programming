0a329735410c609e67a786e3a457298fe8d05bd7
tamirwaleed <hero2000.TS@gmail.com>
2023-11-01 07:02:20 -0700

    teemo

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:    
+    if i != 99:   
         print("{}{}".format(i // 10, i % 10), end=", ")
     else:
         print("{}".format(i))
