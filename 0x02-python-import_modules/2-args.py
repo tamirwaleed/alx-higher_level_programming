afedf891109bc090b28b0eef1936af78ed999dc9
tamirwaleed <hero2000.TS@gmail.com>
2023-11-03 09:39:33 -0700

    teemo

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def funno(argv):
+    if len(argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(argv) - 1 == 1:
+        print("1 argument:\n1: {}".format(argv[1]))
+    elif len(argv) - 1 > 1:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
+if __name__ == "__main__":  
+    import sys
+    funno(sys.argv)
