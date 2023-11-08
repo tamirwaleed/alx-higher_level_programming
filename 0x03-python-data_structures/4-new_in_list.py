9209613bd2bdf4b8be554a4b9b336dc17c8b3e22
tamirwaleed <hero2000.TS@gmail.com>
2023-11-07 18:17:35 -0800

    teemo

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if my_list:
-        new_list = my_list
+        new_list = my_list[:]
         if idx < 0 or idx > len(my_list) - 1:
             return (new_list)
         else:
