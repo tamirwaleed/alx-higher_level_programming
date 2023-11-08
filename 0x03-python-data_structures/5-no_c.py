a28dabebeb50e1175bf4d3aa8f4529f06c326ff4
tamirwaleed <hero2000.TS@gmail.com>
2023-11-07 18:23:39 -0800

    teemo

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     if my_string:
         for i in range(len(my_string)):
-            if my_string[i] = 'c' or my_string[i] = 'C':
+            if my_string[i] == 'c' or my_string[i] == 'C':
                 my_string[i] = []
         return (my_string)
