9d711e98685923317c7447183ce167e220af83c2
tamirwaleed <hero2000.TS@gmail.com>
2023-11-07 18:58:52 -0800

    teemo

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for hnai in matrix: 
+    for hnai in matrix:
         for i in hnai:
             print("{:d}".format(i), end=" " if i != hnai[-1] else "")
         print()
