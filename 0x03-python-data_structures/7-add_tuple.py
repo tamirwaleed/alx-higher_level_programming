8f7b42d004d313a6527651235f1f079298e2e70e
tamirwaleed <hero2000.TS@gmail.com>
2023-11-08 07:13:29 -0800

    teemo

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tupa = tuple_a + (0,0)
-    tupb = tuple_b + (0,0)
+    tupa = tuple_a + (0, 0)
+    tupb = tuple_b + (0, 0)
     tupc = tupa[0] + tupb[0], tupa[1] + tupb[1]
     return (tupc)
