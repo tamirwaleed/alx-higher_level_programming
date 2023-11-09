68daa6e71d19fdbcd601c19193d13e744d764d26
tamirwaleed <hero2000.TS@gmail.com>
2023-11-09 14:41:40 -0800

    teemo

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     newl = []
     for x in matrix:
-        newl.append(list(map(lambda x : x**2, x)))
+        newl.append(list(map(lambda x: x**2, x)))
     return (newl)
