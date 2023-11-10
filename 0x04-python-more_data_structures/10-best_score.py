de7f17fa107f42e691198c9a2a9a08b4f473b1dc
tamirwaleed <hero2000.TS@gmail.com>
2023-11-10 05:30:06 -0800

    teemo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max = 0
+    maxo = 0
+    who = ""
     if a_dictionary:
         for x in a_dictionary:
-            if a_dictionary[x] > max:
-                max = a_dictionary[x]
-        return (max)
+            if a_dictionary[x] > maxo:
+                maxo, who = a_dictionary[x], x
+        return (who)
     else: 
         return None
