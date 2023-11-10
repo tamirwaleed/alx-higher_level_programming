bb2872b2cd57b4465e4eb1e9a54e2f7512f1b711
tamirwaleed <hero2000.TS@gmail.com>
2023-11-10 05:17:42 -0800

    teemo

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return(map(lambda x, y: x if a_dictioary[x] > a_dictionary[y] else y, a_dictionary))
+    if a_dictionary:
+        return(map(lambda x, y: x if a_dictioary[x] > a_dictionary[y] else y, a_dictionary))
+    else: 
+        return None
