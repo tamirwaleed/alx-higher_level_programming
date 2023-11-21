8e40a7f6a1d7c2fc9a427e360043eb8a9e93d103
tamirwaleed <hero2000.TS@gmail.com>
2023-11-20 18:14:24 -0800

    teemo

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,6 @@ def safe_print_integer_err(value):
     check = true
     try:
         print("{:d}".format(value))
-        print()
     except TypeError as nope:
         print("Exception: {}".format(nope), file=sys.stderr)
         check = False
