42d9057694260a20e4d907f2b5001a4e3a530a9f
tamirwaleed <hero2000.TS@gmail.com>
2023-11-22 15:31:17 -0800

    teemo

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is task 2"""
+
+
+class Square:
+    """ A class for a square """
+    def __init__(self, size):
+        """ Instantization """
+        self.__size = size
