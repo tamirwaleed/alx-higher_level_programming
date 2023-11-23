29811c1af3d487fbf532b55220d71d1dfb7cdbdf
tamirwaleed <hero2000.TS@gmail.com>
2023-11-22 17:27:47 -0800

    teemo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -49,13 +49,9 @@ class Square:
         s = self.__size
         if s == 0:
             print()
-            return
         else:
-            for i in range(self.__position[1]):
+            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__size):
+                [print(" ", end="") for j in range(0, self.__position[0])]
+                [print("#", end="") for k in range(0, self.__size)]
                 print("")
-            for i in range(s):
-                for k in range(self.__position[0]):
-                    print(" ", end="")
-                for j in range(s):
-                    print("#", end="")
-                print()
