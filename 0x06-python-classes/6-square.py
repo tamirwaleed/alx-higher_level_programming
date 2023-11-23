b96e6627cffcafbc79fc62f7ffc9c5510621ef87
tamirwaleed <hero2000.TS@gmail.com>
2023-11-22 17:37:21 -0800

    teemo

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -21,8 +21,7 @@ class Square:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
