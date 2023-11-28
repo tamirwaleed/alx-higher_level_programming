d2a460852e7fa163fdf647a20073da211aa38d65
tamirwaleed <hero2000.TS@gmail.com>
2023-11-28 14:43:14 -0800

    teemo

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -43,4 +43,6 @@ class Rectangle:
 
     def perimeter(self):
         ''' Perimeter '''
+        if self.width == 0 or self.height == 0:
+            return (0)
         return ((self.width + self.height) * 2)
