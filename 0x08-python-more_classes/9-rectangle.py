2436256c84de932d4200ff2b7eed74d9b28ba8b9
tamirwaleed <hero2000.TS@gmail.com>
2023-11-28 18:03:32 -0800

    teemo

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         ''' Class meth '''
-        return cls(size, size)
+        return Rectangle(size, size)
 
     @property
     def width(self):
