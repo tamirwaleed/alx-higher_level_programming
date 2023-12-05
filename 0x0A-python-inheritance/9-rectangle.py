709393f758960e0d557e6087edb8bec1697445bf
tamirwaleed <hero2000.TS@gmail.com>
2023-12-05 13:11:40 -0800

    teemo

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Geometry module '''
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    ''' Rectangle Class '''
+    def __init__(self, width, height):
+        ''' Initialization '''
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        ''' implements area '''
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        ''' prints '''
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
