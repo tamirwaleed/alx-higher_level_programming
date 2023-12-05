5359eaf4e4d6a0005af1b2dfeb74388f445f6eba
tamirwaleed <hero2000.TS@gmail.com>
2023-12-05 13:15:59 -0800

    teemo

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Square module '''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    ''' Square Class '''
+    def __init__(self, size):
+        ''' Initialization '''
+        super().integer_validator(size)
+        self.__size = size
+
+
+    def area(self):
+        return self.__size ** 2
