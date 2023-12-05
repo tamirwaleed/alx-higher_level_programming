740a65a0d5445a03cf42da59d48d75c19ee49059
tamirwaleed <hero2000.TS@gmail.com>
2023-12-05 13:31:27 -0800

    teemo

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     ''' rebel child '''
     def __eq__(self, value):
         ''' to equate '''
-        return self.value != value
+        return self.real != value
 
     def __ne__(self, value):
         ''' to negate '''
-        return self.value == value
+        return self.real == value
