2b198cfe3fd10e0c43bedbe4558792a0c0286de1
tamirwaleed <hero2000.TS@gmail.com>
2023-12-05 13:39:33 -0800

    teemo

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,7 +3,13 @@
 
 
 def addattrib_(obj, attr, value):
-    ''' the function '''
+    ''' the function 
+    Args:
+        obj (any)
+        attr (string)
+        value (any)
+    Raises:
+        TypeError if could not be added'''
     if not hasattr(obj, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(obj, attr, value)
