0038e704a80a69e9880e1a114dabe6041c729e11
tamirwaleed <hero2000.TS@gmail.com>
2023-12-06 07:28:21 -0800

    teemo

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' Writing module '''
+
+
+def write_file(filename="", text=""):
+    ''' The function '''
+    with open(filename, 'w') as fd:
+        fd.write(text)
--- 0x0B-python-input_output/tst.py
+++ 0x0B-python-input_output/tst.py
@@ -2,3 +2,7 @@
 read_file = __import__('0-read_file').read_file
 
 read_file("my_file_0.txt")
+write_file = __import__('1-write_file').write_file
+
+nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+print(nb_characters)
