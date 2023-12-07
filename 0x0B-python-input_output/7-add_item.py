ffd9c98a1b561566b60678d1eac673de8ec1a514
tamirwaleed <hero2000.TS@gmail.com>
2023-12-06 18:17:13 -0800

    teemo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
             __import__('5-save_to_json_file').save_to_json_file
 
     load_from_json_file = \
-            __import__('6-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         whatev = load_from_json_file('add_item.json')
