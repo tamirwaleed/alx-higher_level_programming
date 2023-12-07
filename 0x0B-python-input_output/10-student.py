14ecde23db5a806c8ae715a8075f43c16d0b5880
tamirwaleed <hero2000.TS@gmail.com>
2023-12-06 18:49:47 -0800

    teemo

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -14,6 +14,6 @@ class Student:
     def to_json(self, attrs=None):
         ''' dictionary '''
         if (type(attrs) == list and
-            all(type(elem) == str for elem in attrs)):
+                all(type(elem) == str for elem in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
