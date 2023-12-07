6b844abb16e45dede90ab2d2914ab03c97ccc6fc
tamirwaleed <hero2000.TS@gmail.com>
2023-12-07 07:34:18 -0800

    teemo

--- 0x0B-python-input_output/tst.py
+++ 0x0B-python-input_output/tst.py
@@ -1,30 +1,17 @@
 #!/usr/bin/python3
-to_json_string = __import__('3-to_json_string').to_json_string
+"""
+12-main
+"""
+pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
-my_list = [1, 2, 3]
-s_my_list = to_json_string(my_list)
-print(s_my_list)
-print(type(s_my_list))
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
-my_dict = { 
-    'id': 12,
-    'name': "John",
-    'places': [ "San Francisco", "Tokyo" ],
-    'is_active': True,
-    'info': {
-        'age': 36,
-        'average': 3.14
-    }
-}
-s_my_dict = to_json_string(my_dict)
-print(s_my_dict)
-print(type(s_my_dict))
 
-try:
-    my_set = { 132, 3 }
-    s_my_set = to_json_string(my_set)
-    print(s_my_set)
-    print(type(s_my_set))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
 
