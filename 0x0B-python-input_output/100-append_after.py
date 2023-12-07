077679c25864aaf6b22498a09ac7d3bc5fa80c57
tamirwaleed <hero2000.TS@gmail.com>
2023-12-07 07:40:00 -0800

    teemo

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' APPENDS AFTER '''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    ''' the function '''
+    text = ""
+    with open(filename) as fd:
+        for line in fd:
+            text = text + line
+            if search_string in line:
+                text = text + search_string
+    with open(filename, 'w') as fg:
+        fg.write(text)
