a914496a97bed10f63259adb9b6fdf9ce8366b37
tamirwaleed <135042104+tamirwaleed@users.noreply.github.com>
2023-12-31 03:58:57 +0400

    Delete 0x09-python-everything_is_object directory

--- 0x09-python-everything_is_object/101-locked_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-''' Locked '''
-
-
-class LockedClass:
-    ''' Class '''
-    __slots__ = "first_name"
--- 0x09-python-everything_is_object/19-copy_list.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def copy_list(lt):
-    return ([x for x in lt])
