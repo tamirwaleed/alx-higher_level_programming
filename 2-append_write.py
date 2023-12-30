b577851487b02bc148d62d4bd4417ebe6138cea1
tamirwaleed <135042104+tamirwaleed@users.noreply.github.com>
2023-12-31 03:59:42 +0400

    Delete 2-append_write.py

--- 2-append_write.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-''' Appending module '''
-
-
-def append_write(filename="", text=""):
-    ''' The Function '''
-    with open(filename, 'a') as fd:
-        return (fd.append(text))
-
