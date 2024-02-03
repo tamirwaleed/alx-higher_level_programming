d62f5cea56c40c7c6e329213b3a87111c6abeccd
tamirwaleed <hero2000.ts@gmail.com>
2024-02-03 07:54:49 -0800

    teemo

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    global stg = ''
-    return stg.append('BestSchool')
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
