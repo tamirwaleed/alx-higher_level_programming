b85c3ee9164c4eaa50e3688679ffbf1a5b7d5dd8
tamirwaleed <hero2000.ts@gmail.com>
2024-02-05 15:12:05 -0800

    teemo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""" Another one """
 
 
 def inherits_from(obj, a_class):
     """ direct subclass """
-    return type(obj) != a_class and isinstance(obj, a_class)   
+    return type(obj) != a_class and isinstance(obj, a_class)
