8fa927789d320bdc0e30e69c164e46326df9d1ae
tamirwaleed <hero2000.ts@gmail.com>
2024-02-05 10:09:59 -0800

    teemo

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Second module """
+
+
+class MyList(list):
+    """ my new class list """
+
+    def print_sorted(self):
+        """ sorted list """
+        print(self.sort())
