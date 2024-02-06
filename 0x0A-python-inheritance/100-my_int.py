f2217d3a8fc22f64899e172646fe6f11e3c454cc
tamirwaleed <hero2000.ts@gmail.com>
2024-02-05 17:03:08 -0800

    teemo

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,8 +6,8 @@ class MyInt(int):
     """ bizarro integer """
     def __eq__(self, value):
         """ changes equality """
-        return self.real != value
+        return int(self) != value
 
     def __ne__(sef, value):
         """ changes non equality """
-        return self.real == value
+        return int(self)  == value
---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" can i??? """
+
+
+def 
