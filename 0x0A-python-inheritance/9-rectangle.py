c409a12823fe5946eaeabfb62e9e1977c430cdd0
tamirwaleed <hero2000.ts@gmail.com>
2024-02-05 16:31:56 -0800

    teemo

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -20,4 +20,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """ string rep """
-        return("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
