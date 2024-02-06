d2fbd51d5d473da2215058704fb6d97d8305c20d
tamirwaleed <hero2000.ts@gmail.com>
2024-02-05 16:59:46 -0800

    teemo

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" a new integer """
+
+
+class MyInt(int):
+    """ bizarro integer """
+    def __eq__(self, value):
+        """ changes equality """
+        return self.real != value
+
+    def __ne__(sef, vaue):
+        """ changes non equality """
+        return self.real == value
