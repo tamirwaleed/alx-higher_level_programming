55d08e662f8602805cffc45d7fe50f15e30d54db
tamirwaleed <hero2000.ts@gmail.com>
2024-02-07 06:09:26 -0800

    teemo

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Third module """
+
+
+def append_write(filename="", text=""):
+    """ appends to file """
+    with open(filename, "a") as fd:
+        return fd.append(text)
