165281afb8c4a0ba4083f344dd9d2db2fbb224fa
tamirwaleed <hero2000.ts@gmail.com>
2024-02-07 07:31:22 -0800

    teemo

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,13 @@ import sys
 
 
 if __name__ == "__main__":
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+            __import__('6-load_from_json_file').load_from_json_file
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     reqlist = []
     try:
         jk = load_from_json_file("add_item.json")
-    except:
+    except FileNotFoundError:
         jk = []
     jk.extend(sys.argv[1:])
     save_to_json_file(jk, "add_item.json")
