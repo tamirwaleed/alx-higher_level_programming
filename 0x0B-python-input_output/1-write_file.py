dc7c58d4a43375448e7c361db34282d51e6523ce
tamirwaleed <hero2000.ts@gmail.com>
2024-02-07 06:07:35 -0800

    teemo

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,4 @@
 def write_file(filename="", text=""):
     """ write file """
     with open(filename, "w") as fd:
-        fd.write(text)
-        for i,text in enumerate(text):
-            pass
-        return i
+        return (fd.write(text))
