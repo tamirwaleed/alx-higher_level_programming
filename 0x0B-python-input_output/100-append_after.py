8d10940581290bcf8947d0f5c191a034ae32c42e
tamirwaleed <hero2000.ts@gmail.com>
2024-02-08 10:28:25 -0800

    teemo

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Advanced modules """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ the function """
+    with open(filename, "r+") as fd:
+        i = 0
+        fileread = fd.readlines()
+        for line in fileread:
+            if search_string in line:
+                fileread.insert(i + 1, new_string)
+            i += 1
+        fd.seek(0)
+        fd.write("".join(fileread))
+    return fd
