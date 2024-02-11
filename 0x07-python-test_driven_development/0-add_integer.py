7ad1cb7363f088bab71a119795bd4db3b16bd5cd
tamirwaleed <hero2000.ts@gmail.com>
2024-02-11 13:16:01 -0800

    teemo

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" The add module """
+
+
+def add_integer(a, b=98):
+    """ the function """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
