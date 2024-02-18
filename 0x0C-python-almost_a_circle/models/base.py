cd73c47e06ad3b532b8ef709c02332967f3bed6a
tamirwaleed <hero2000.ts@gmail.com>
2024-02-18 10:11:55 -0800

    teemo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,10 @@
 class Base:
     ''' this is the class '''
     __nb_objects = 0
+
     def __init__(self, id=None):
         ''' the class constructor '''
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
