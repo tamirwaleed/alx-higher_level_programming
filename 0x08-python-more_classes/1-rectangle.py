d74b87ffd49bad0e8816d1d3146abebebd001645
Tamir <hero2000.ts@gmail.com>
2024-04-27 09:05:52 -0700

    teemo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """ the class """
     def __init__(self, width = 0, height = 0):
         """ instantiation """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
     
     @property
     def height(self):
