b2b49a2fe034a90d9dc07f91c7423bf4977e1ea2
Tamir <hero2000.ts@gmail.com>
2024-04-27 07:52:25 -0700

    teemo

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" The Rectangle module """
+
+
+class Rectangle:
+    """ the class """
+    def __init__(self, width = 0, height = 0):
+        """ instantiation """
+        self.__width = width
+        self.__height = height
+    
+    @property
+    def height(self):
+        """ getter """
+        return self.__height
+    
+    @property
+    def width(self):
+        """ getter """
+        return self.__width
+    
+    @height.setter
+    def height(self, value):
+        """ setter """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+    @width.setter
+     def width(self, value):
+        """ setter """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
