1b67a9fdfc0a6cd665f270d914c2d3114cb7698e
Tamir <hero2000.ts@gmail.com>
2024-05-01 04:20:59 -0700

    teemo

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A function that returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """ the function """
+    return list(obj.__dict__)
