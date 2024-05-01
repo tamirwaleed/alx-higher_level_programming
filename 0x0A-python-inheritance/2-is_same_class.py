d1e52c5ced1ac274dd123fdfb559555e1145f060
Tamir <hero2000.ts@gmail.com>
2024-05-01 06:58:02 -0700

    teemo

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" checks if the object is an INSTANCE """
+
+
+def is_same_class(obj, a_class):
+    """ the function """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
