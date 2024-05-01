9f5631486a7289dc80f3c6549f6699aef6eb4654
Tamir <hero2000.ts@gmail.com>
2024-05-01 08:22:57 -0700

    teemo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" the geometry base module """
+
+
+class BaseGeometry:
+    """ class defined """
+    def area(self):
+        """ Finds the area """
+        raise Exception("area() is not implemented")
