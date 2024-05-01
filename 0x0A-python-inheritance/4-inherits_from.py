51f4d4c093d620d979e2a959854c4b74d5360f71
Tamir <hero2000.ts@gmail.com>
2024-05-01 07:20:59 -0700

    teemo

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" checks if the object is an INSTANCE 
+""" checks if the object is an INSTANCE
 of a certain CLASS"""
 
 
 def inherits_from(obj, a_class):
     """ the function returns True or False"""
-    if type(obj) != a_class and isinstance(type(obj), a_class):
+    if type(obj) != a_class and isinstance(obj, a_class):
         return True
     else:
         return False
