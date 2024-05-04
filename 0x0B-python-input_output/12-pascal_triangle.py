5fc269ffcc1b00b39cfb4150d028d7dd69a0c556
Tamir <hero2000.ts@gmail.com>
2024-05-04 08:57:32 -0700

    teemo

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" returns a list of pascal triangle """
+
+
+def pascal_triangle(n):
+    """ n is the triangle level """
+    pasc = []
+    lev = []
+    if n <= 0:
+        return pasc
+    else:
+        for i in range(n):
+            temp = lev[:]
+            lev = [1]
+            for j in range(1, i + 1):
+                if j == i:
+                    lev.append(1)
+                else:
+                    lev.append(temp[j] + temp[j - 1])
+            pasc.append(lev)
+    return pasc[:]
+
+
+
