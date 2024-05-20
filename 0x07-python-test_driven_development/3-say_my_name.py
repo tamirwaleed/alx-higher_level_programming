032ac999f8cd0c8618a63cd9e972138a66133ade
Tamir <hero2000.ts@gmail.com>
2024-05-20 13:39:28 -0700

    teemo

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,8 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """ the function """
-    if type(first_name) != str:
+    if type(first_name) is not str:
         raise TypeError("first_name must be a string")
-    if type(last_name) != str:
+    if type(last_name) is not str:
         raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
