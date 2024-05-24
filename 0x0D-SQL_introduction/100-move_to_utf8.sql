da447c607b2411ad936e41adcffe11833b3773a2
Tamir <hero2000.ts@gmail.com>
2024-05-24 12:21:33 -0700

    teemo

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- convert encoding
 USE `hbtn_0c_0`
-ALTER TABLE second_table 
-CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table 
+CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
