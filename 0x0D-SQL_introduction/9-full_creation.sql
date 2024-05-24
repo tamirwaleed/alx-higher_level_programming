d3bde5d877fd4aaf2e851de64f09fd7601c7f87c
Tamir <hero2000.ts@gmail.com>
2024-05-24 11:40:47 -0700

    teemo

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,9 +7,9 @@ CREATE TABLE second_table(
 	score INT
 );
 
-INSERT INTO second_table(
+INSERT INTO second_table
+(id, name, score)
 	VALUES(1, "John", 10),
 	VALUES(2, "ALex", 3),
 	VALUES(3, "Bob", 14),
-	VALUES(4, "Geoge", 8)
-);
+	VALUES(4, "Geoge", 8);
