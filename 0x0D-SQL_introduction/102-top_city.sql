40b4b38cdff38718f9a52eb7ac1d5527fa32ce03
Tamir <hero2000.ts@gmail.com>
2024-05-24 12:30:14 -0700

    teemo

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,10 @@
 -- avg temp
 SELECT city, AVG(value) as avg_temp
-FROM temperatures
+FROM (
+	SELECT *
+	FROM temperatures
+	WHERE month = "July" or month = "August"
+)
 GROUP BY city
-HAVING month = "July" or month = "August"
 ORDER BY avg_temp DESC
 LIMIT 3;
