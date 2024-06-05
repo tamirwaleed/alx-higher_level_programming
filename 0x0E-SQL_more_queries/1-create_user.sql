be6c1eac9db9bf80420237f59553ba1bf6ce248a
Tamir <hero2000.ts@gmail.com>
2024-06-05 07:06:09 -0700

    teemo

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 --creates new user
-CREATE USER IF NOT EXISTS'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER 
+	IF NOT EXISTS 'user_0d_1'@'localhost' 
+	IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES 
+	ON *.* 
+	TO 'user_0d_1'@'localhost';
