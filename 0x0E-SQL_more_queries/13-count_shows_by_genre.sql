b81c6edf7b755fcc2d293024a63b7e8f6c581690
Tamir <hero2000.ts@gmail.com>
2024-06-05 09:05:13 -0700

    teemo

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,14 @@
+-- creates table
+SELECT 
+	tv_genres.name as genre,
+	COUNT(tv_show_genres.genre_id) AS number_of_shows
+FROM 
+	tv_show_genres
+LEFT JOIN
+	tv_genres
+ON 
+	tv_show_genres.genre_id = tv_genres.id
+GROUP BY
+	tv_show_genres.genre_id	
+ORDER BY
+	number_of_shows DESC;
