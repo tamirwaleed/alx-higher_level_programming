9ac19b8d72d551aeb89c05f0505dc22429bac41e
Tamir <hero2000.ts@gmail.com>
2024-06-05 08:00:32 -0700

    teemo

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,10 +1,10 @@
 -- creates table
 SELECT 
-	tv_shows.title, tv_shows_genres.genre_id
+	tv_shows.title, tv_show_genres.genre_id
 FROM 
 	tv_shows,tv_show_genres
 WHERE
 	tv_shows.id = tv_show_genres.show_id
 ORDER BY
 	tv_shows.title,
-	tv_shows_genres.genre_id;
+	tv_show_genres.genre_id;
