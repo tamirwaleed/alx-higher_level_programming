45677a901385cdfb4b20228c3a93f3dac9cb349d
Tamir <hero2000.ts@gmail.com>
2024-06-05 11:44:42 -0700

    teemo

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,11 +4,11 @@ SELECT
 	tv_genres.name AS name
 FROM
 	tv_shows
-INNER JOIN 
+LEFT JOIN 
 	tv_show_genres
 ON
 	tv_shows.id = tv_show_genres.show_id
-INNER JOIN
+LEFT JOIN
 	tv_genres
 ON
 	tv_genres.id = tv_show_genres.genre_id
