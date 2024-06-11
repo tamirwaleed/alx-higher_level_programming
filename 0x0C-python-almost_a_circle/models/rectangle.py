b805f734429ab99c71156e06aca83d5010227567
Tamir <hero2000.ts@gmail.com>
2024-06-11 07:26:15 -0700

    teemo

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,9 +15,6 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-    def area(self):
-        """ returns the area """
-        return self.__width * self__height
     @property
     def width(self):
         """ getter """
@@ -73,3 +70,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ returns the area """
+        return self.__width * self__height
