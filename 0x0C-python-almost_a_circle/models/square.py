0801ff8b512aa4dbdce4436ceb6b46a88d52d5a8
Tamir <hero2000.ts@gmail.com>
2024-06-12 05:56:51 -0700

    teemo

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,7 +30,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 self.__setattr__(key, value)
-    
+
     @property
     def size(self):
         """ getter """
