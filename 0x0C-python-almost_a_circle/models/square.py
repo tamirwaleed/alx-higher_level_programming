c229070c14e756076cd9c1ce2a953bc9a358891e
Tamir <hero2000.ts@gmail.com>
2024-06-12 05:33:00 -0700

    teemo

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" The square module """
+
+
+from models.base import Rectangle
+
+
+class Square(Rectangle):
+    """ the class """
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Instantization """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ returns string represenation """
+        line1 = "[Square] ({}) {}/{}".format(self.id, self.x, self.y)
+        line1 += " - {}".format(self.width)
+        return line1
