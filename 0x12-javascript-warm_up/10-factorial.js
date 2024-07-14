474df4eb38149c36f3dc1f9f49b5c0ef9adf096a
Tamir <hero2000.ts@gmail.com>
2024-07-14 12:40:51 -0700

    Another ALX Task

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,7 +3,7 @@ function factorial (n = parseInt(process.argv[2])) {
   if (isNaN(n) || n === 1) {
     return 1;
   } else {
-      return n * factorial(n - 1);
+    return n * factorial(n - 1);
   }
 }
 
