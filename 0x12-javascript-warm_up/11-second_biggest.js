2275c8e6e007db52af8fbf8beef7ade4edb6a9a3
Tamir <hero2000.ts@gmail.com>
2024-07-14 13:08:28 -0700

    Another ALX Task

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,23 +3,18 @@ function secondBig () {
   if (process.argv.length < 3) {
     console.log(0);
   } else {
-      let i = process.argv[2];
-      let a;
-      let b;
-      for (let j = 2; j < process.argv.length; j++) {
-        a = process.argv[j];
-        if (a > i) {
-          i = process.argv[j];
-        }
-      }
-      b = process.argv[2];
-      for (j = 2; j < process.argv.length; j++) {
-        a = process.argv[j];
-        if (a < i && a > b) {
-          b = process.argv[j];
+    let i = process.argv[2];
+    let a;
+    let b = process.argv[2];
+    for (let j = 2; j < process.argv.length; j++) {
+      a = process.argv[j];
+      if (a > i) {
+        i = process.argv[j];
+      } else if (a < i && a > b) {
+        b = process.argv[j];
       }
     }
-      return b;
+    return b;
   }
 }
 console.log(secondBig());
