45c20d006fe051ab2641996e0f3984360b5a792a
Tamir <hero2000.ts@gmail.com>
2024-07-14 10:47:03 -0700

    Another ALX Task

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-let myRow = ''
+let myRow = '';
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-      myRow += 'X' 
+    myRow += 'X';
   }
   for (let j = 0; j < parseInt(process.argv[2]); j++) {
-      console.log(myRow);
+    console.log(myRow);
   }
 }
