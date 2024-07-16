6307ed4b33341b9f6df0689ccafb1f7a02aa5f99
Tamir <hero2000.ts@gmail.com>
2024-07-16 06:30:59 -0700

    Another ALX Task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,7 +2,7 @@
 const myArray = require('./100-data').list;
 let i = 0;
 const newList = myArray.map((x) => {
-	return (i++ * parseInt(x));
+  return (i++ * parseInt(x));
 });
 console.log(myArray);
 console.log(newList);
