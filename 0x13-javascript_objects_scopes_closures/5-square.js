c824df17b598e3da4980f9ba974500b03601abef
Tamir <hero2000.ts@gmail.com>
2024-07-16 04:31:23 -0700

    Another ALX Task

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,6 +3,7 @@ const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
   constructor (size) {
+    this.size = size;
     super(constructor(size, size));
   }
 };
