ba687da7cf3aba91b5394c0a8c79ed9a05654f1a
Tamir <hero2000.ts@gmail.com>
2024-07-16 04:44:40 -0700

    Another ALX Task

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square_1 = require('./5-square');
 
-module.exports = class Square extends Rectangle {
+module.exports = class Square extends Square_1 {
   constructor (size) {
-    super(size, size);
+    super(size);
   }
 
   charPrint (c) {
