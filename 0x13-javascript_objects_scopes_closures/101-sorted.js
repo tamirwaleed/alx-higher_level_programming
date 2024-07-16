7ad45242690652a3d22edc78a0a3615ca0d5d520
Tamir <hero2000.ts@gmail.com>
2024-07-16 07:00:45 -0700

    Another ALX Task

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,9 +2,9 @@
 const myDict = require('./101-data').dict;
 const newDict = {};
 for (const N in myDict) {
-	    if (newDict[myDict[N]] === undefined) {
-		    	newDict[myDict[N]] = [];
-		        }
-	    newDict[myDict[N]].push(N);
+  if (newDict[myDict[N]] === undefined) {
+    newDict[myDict[N]] = [];
+  }
+  newDict[myDict[N]].push(N);
 }
 console.log(newDict);
