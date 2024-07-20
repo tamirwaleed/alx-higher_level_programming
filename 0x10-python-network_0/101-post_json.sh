b9d923b12c725ae52ca491ee4aef16fa092c2314
Tamir <hero2000.ts@gmail.com>
2024-07-20 12:55:11 -0700

    Another ALX Task

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -d "$(cat $2)" $1
+curl -s -H "Content-type: application/json" -d "$(cat $2)" $1
