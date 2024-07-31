9e09e667122f9fd32d75f32a0762f4a7a079f1ee
Tamir <hero2000.ts@gmail.com>
2024-07-31 08:14:01 -0700

    Another ALX Task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,8 +3,8 @@
 const fs = require('fs');
 const filepath = process.argv[2];
 try {
-	const theData = fs.readFileSync(filepath, 'utf8');
-	console.log(theData);
+  const theData = fs.readFileSync(filepath, 'utf8');
+  console.log(theData);
 } catch (err) {
-	console.log(err);
-};
+  console.log(err);
+}
