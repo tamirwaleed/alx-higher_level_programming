3a460c6719a538f6ab79316907698d8d9b91c5cd
Tamir <hero2000.ts@gmail.com>
2024-07-31 11:43:36 -0700

    Another ALX Task

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    console.log(body.get("title"));
+  }
+});
