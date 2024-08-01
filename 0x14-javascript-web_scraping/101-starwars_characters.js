4eb821bdab79d059956832f026ba4fe55047502d
Tamir <hero2000.ts@gmail.com>
2024-08-01 11:03:36 -0700

    Another ALX Task

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,25 @@
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+function printInOrder (chars, i) {
+  request(chars[i], (error, response, body) => {
+    if (error) {
+      console.log('error:', error);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (i + 1 < chars.length) {
+        printInOrder(chars, i + 1);
+      }
+    }
+  });
+}
+
 request(url, (error, response, body) => {
   if (error) {
     console.log('error:', error);
   } else {
     const chars = JSON.parse(body).characters;
-    for (let i = 0; i < chars.length; i++) {
-      request(chars[i], (error, response, body) => {
-        if (error) {
-          console.log('error:', error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    printInOrder(chars, 0);
   }
 });
