a79886ee766ab0d95f8ee58f2d898ad8e8dcce6b
Tamir <hero2000.ts@gmail.com>
2024-08-01 08:03:36 -0700

    Another ALX Task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,17 +2,4 @@
 
 const fs = require('fs');
 const request = require('request');
-const url = process.argv[2];
-const filepath = process.argv[3];
-request(url, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    const content = console.log(JSON.parse(body));
-    try {
-      fs.writeFileSync(filepath, content, 'utf8');
-    } catch (err) {
-      console.error(err);
-    }
-  }
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
