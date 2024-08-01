f9d2170b15fd1ce4d3049d3fe0306d95350c62ed
Tamir <hero2000.ts@gmail.com>
2024-08-01 05:12:23 -0700

    Another ALX Task

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let counter = 0;
+const wedge = 'https://swapi-api.alx-tools.com/api/people/18'
+request(url, (error, response, body) => {
+  if (error) {
+    console.log('error:', error);
+  } else {
+    const results = JSON.parse(body).results;
+    for (i = 0; i < results.length; i++) {
+      if (results[i]['characters'].includes(wedge)) {
+        counter++;
+      }
+    }
+    console.log(counter);
+  }
+});
