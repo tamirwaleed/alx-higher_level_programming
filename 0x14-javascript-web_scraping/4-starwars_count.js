29b94a9431b0558d8e85c4a0a383ef9459bd1d9f
Tamir <hero2000.ts@gmail.com>
2024-08-01 07:48:55 -0700

    fix no.4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,15 +3,16 @@
 const request = require('request');
 const url = process.argv[2];
 let counter = 0;
-const wedge = 'http://swapi-api.alx-tools.com/api/people/18/';
 request(url, (error, response, body) => {
   if (error) {
     console.log('error:', error);
   } else {
-    const results = JSON.parse(body).results;
+    const results = JSON.parse(body)['results'];
     for (let i = 0; i < results.length; i++) {
-      if (results[i].characters.includes(wedge)) {
-        counter++;
+      for (let j = 0; j < results[i]['characters'].length; j++) {       
+        if (results[i]['characters'][j].endsWith('18/')) {
+          counter++;
+        }
       }
     }
     console.log(counter);
