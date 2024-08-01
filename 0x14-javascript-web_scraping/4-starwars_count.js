d4d19c2483c096ba234281e7124379767a9520a9
Tamir <hero2000.ts@gmail.com>
2024-08-01 07:49:49 -0700

    Another ALX Task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,10 +7,10 @@ request(url, (error, response, body) => {
   if (error) {
     console.log('error:', error);
   } else {
-    const results = JSON.parse(body)['results'];
+    const results = JSON.parse(body).results;
     for (let i = 0; i < results.length; i++) {
-      for (let j = 0; j < results[i]['characters'].length; j++) {       
-        if (results[i]['characters'][j].endsWith('18/')) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j].endsWith('18/')) {
           counter++;
         }
       }
