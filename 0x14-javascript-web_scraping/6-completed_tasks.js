23ffc3f613f02eb580733ca0ac85ffe68fe8c320
Tamir <hero2000.ts@gmail.com>
2024-08-01 08:58:55 -0700

    Another ALX Task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -11,12 +11,12 @@ request(url, (error, response, body) => {
     const results = JSON.parse(body);
     let userId = results[0].userId;
     for (let i = 0; i < results.length; i++) {
-      if (results[i].completed === true) {
+      if (results[i].completed) {
         if (finale[results[i].userId] === userId) {
           counter++;
         } else if (finale[results[i].userId] !== userId) {
           finale[userId] = counter;
-          userId = finale[results[i].userId];
+          userId = results[i].userId;
           finale[userId] += 1;
           counter = 0;
         } else {
