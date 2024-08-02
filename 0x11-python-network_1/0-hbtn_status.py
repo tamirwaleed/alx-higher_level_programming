b3bf6aa0da129f7a0e0182a77ac1852ae7ee78a3
Tamir <hero2000.ts@gmail.com>
2024-08-02 08:19:45 -0700

    Another ALX Task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,4 +9,4 @@ with urllib.request.urlopen(url) as response:
     html = response.read()
     print('Body response:\n\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
-    print('\t- utf8 content: OK')
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
