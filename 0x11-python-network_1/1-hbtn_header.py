34b312301d8d8f31c2ca2872682494285c5643a9
Tamir <hero2000.ts@gmail.com>
2024-08-02 08:47:22 -0700

    Another ALX Task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''X-Request-Id variable found in the header of the response'''
 
-
 import sys
 import urllib.request
+
 
 url = sys.argv[1]
 with urllib.request.urlopen(url) as response:
