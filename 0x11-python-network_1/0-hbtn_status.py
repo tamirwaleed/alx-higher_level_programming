846a71686356a0d6c90ba589a5eccb5f052c2af3
Tamir <hero2000.ts@gmail.com>
2024-08-02 08:16:55 -0700

    Another ALX Task

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Write a Python script that fetches https://alx-intranet.hbtn.io/status '''
+
+import urllib.request
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print('Body response:\n\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: OK')
