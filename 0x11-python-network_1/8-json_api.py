55901f2989b6bf4ba5a09517ba0658a52b15457a
Tamir <hero2000.ts@gmail.com>
2024-08-03 05:50:27 -0700

    Another ALX Task

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Sends a POST request to an email'''
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if sys.argv[2]:
+        data = {'q': sys.argv[2]}
+    else:
+        data = {'q': ""}
+    r = requests.post(url, data={'q': sys.argv[2]})
+    try:
+    r.json()
+    print("[{}] {}".format(r.headers.get('id'), r.headers.get('name')))
+    except:
+        if r.status_code == 204:
+            print("No result")
+        else:
+            print("Not a valid JSON")
