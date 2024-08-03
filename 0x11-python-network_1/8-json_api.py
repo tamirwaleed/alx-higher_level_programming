cb80de76024df1fd4fc8d6a282911e441646f19f
Tamir <hero2000.ts@gmail.com>
2024-08-03 06:06:15 -0700

    Another ALX Task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
         r = requests.post(url, data={'q': ""})
     try:
         r = r.json()
-        if len(r) > 0 && r.get('id') && r.get('name'):
+        if len(r) > 0 and r.get('id') and r.get('name'):
             print("[{}] {}".format(r.get('id'), r.get('name')))
         else:
             print("No result")
-    except:
+    except Exception as e:
         print("Not a valid JSON")
