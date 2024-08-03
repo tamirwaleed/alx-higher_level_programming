795fff0c43c4e19b68f79d3398305d64d11d4c2e
Tamir <hero2000.ts@gmail.com>
2024-08-03 05:54:42 -0700

    Another ALX Task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -8,10 +8,9 @@ import sys
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
     if len(sys.argv) == 2:
-        data = {'q': sys.argv[1]}
+        r = requests.post(url, data={'q': sys.argv[1]})
     else:
-        data = {'q': ""}
-    r = requests.post(url, data={'q': sys.argv[1]})
+        r = requests.post(url, data={'q': ""})
     try:
         r.json()
         print("[{}] {}".format(r.headers.get('id'), r.headers.get('name')))
