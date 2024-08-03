20f246704890e2889465a0c06f11bd0669e609e4
Tamir <hero2000.ts@gmail.com>
2024-08-03 05:01:14 -0700

    Another ALX Task

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Sends a POST request to an email'''
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    r = requests.post(url, data={'email': sys.argv[2]})
+    r = requests.get(url)
+    print(r.text)
