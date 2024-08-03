c68dc50e9c8e428482039395dd0d2d4292d828b3
Tamir <hero2000.ts@gmail.com>
2024-08-03 06:43:43 -0700

    Another ALX Task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,13 +2,14 @@
 '''Sends a POST request to an email'''
 
 import requests
+import requests.auth
 import sys
 
 
 if __name__ == "__main__":
-    url = 'https://api.github.com/user'
     username = sys.argv[1]
     pswd = sys.argv[2]
-    data={'username': username, 'password': pswd}
-    r = requests.post(url, data=data)
+    url = 'https://api.github.com/user/' + username
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(username, pswd))
     print(r.json().get('id'))
