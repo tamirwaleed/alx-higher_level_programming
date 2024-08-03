50663e2eaef6d63b62a569e2f61fb21ed45a7d4a
Tamir <hero2000.ts@gmail.com>
2024-08-03 02:35:28 -0700

    Another ALX Task

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,6 +5,7 @@ import sys
 import urllib.request
 
 
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    print(dict(response.headers).get('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(dict(response.headers).get('X-Request-Id'))
