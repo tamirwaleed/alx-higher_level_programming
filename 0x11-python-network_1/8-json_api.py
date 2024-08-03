b77b07e9d2705a44fe9c14a3a9afbcf04e735e5c
Tamir <hero2000.ts@gmail.com>
2024-08-03 05:52:40 -0700

    Another ALX Task

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__ == "__main__":
     url = 'http://0.0.0.0:5000/search_user'
-    if len(sys.argv == 2):
+    if len(sys.argv) == 2:
         data = {'q': sys.argv[1]}
     else:
         data = {'q': ""}
