a7abeb5aa8ffa94bf4f1e97627b1eee98b63029a
Tamir <hero2000.ts@gmail.com>
2024-08-03 05:06:27 -0700

    Another ALX Task

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     r = requests.get(url)
     if (r.status_code >= 400):
-        print('Error code: {}'.format(r,status_code))
+        print('Error code: {}'.format(r.status_code))
     else:
         print(r.text)
