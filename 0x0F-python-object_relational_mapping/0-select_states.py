0e6e53879305ba5ce928d91a6a1108c04ba2fd39
Tamir <hero2000.ts@gmail.com>
2024-08-05 11:02:17 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,5 +14,7 @@ if __name__ == "__main__":
     cur = con.cursor()
     cur.execute("SELECT * FROM states")
     results = cur.fetchall()
+    for i in results:
+        print(i)
     cur.close()
     con.close()
