5c97417b27b724f5c2cef82614d3ad5c5fa5be74
Tamir <hero2000.ts@gmail.com>
2024-08-05 10:07:55 -0700

    Another ALX Task

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' lists all states from the database hbtn_0e_0_usa '''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host='localhost',
+                          port=3306,
+                          user=sys.argv[1],
+                          passwd=sys.argv[2],
+                          db=sys.argv[3])
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states")
+    results = cur.fetchall()
+    cur.close()
+    con.close()
