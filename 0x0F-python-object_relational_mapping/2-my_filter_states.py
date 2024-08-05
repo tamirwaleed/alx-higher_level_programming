bbf09f508413955256c2b18500224b595e09ff07
Tamir <hero2000.ts@gmail.com>
2024-08-05 12:14:59 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     cur = con.cursor()
     cur.execute("SELECT * \
                 FROM states \
-                WHERE name like '{}' \
+                WHERE name like BINARY '{}' \
                 ORDER BY id".format(sys.argv[4]))
     results = cur.fetchall()
     for i in results:
