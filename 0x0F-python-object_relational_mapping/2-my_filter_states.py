bbe85fac204a62f1522c2fef1ceebd3898631ec6
Tamir <hero2000.ts@gmail.com>
2024-08-05 12:14:07 -0700

    Another ALX Task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' lists all states from the database hbtn_0e_0_usa (filtered)'''
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    con = MySQLdb.connect(host='localhost',
+                          port=3306,
+                          user=sys.argv[1],
+                          passwd=sys.argv[2],
+                          db=sys.argv[3])
+    cur = con.cursor()
+    cur.execute("SELECT * \
+                FROM states \
+                WHERE name like '{}' \
+                ORDER BY id".format(sys.argv[4]))
+    results = cur.fetchall()
+    for i in results:
+        print(i)
+    cur.close()
+    con.close()
