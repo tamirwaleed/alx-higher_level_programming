ed1bd27a08816a79c6fe4579934e78d7f94cb11a
Tamir <hero2000.ts@gmail.com>
2024-08-06 16:02:22 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -2,7 +2,7 @@
 ''' file that contains the class definition of a State
 and an instance Base = declarative_base() '''
 
-from model_state import Base, State
+from relationship_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
@@ -14,4 +14,3 @@ class City(Base):
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
-    state = relationship("State", back_populates="cities")
