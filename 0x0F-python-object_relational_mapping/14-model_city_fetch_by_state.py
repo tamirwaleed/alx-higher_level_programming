8d4eba59779d0163e3a0efb1804df982e5b08a0c
Tamir <hero2000.ts@gmail.com>
2024-08-06 09:10:31 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa
+"""lists all City objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
@@ -14,12 +15,12 @@ if __name__ == "__main__":
             sys.argv[3]),
             pool_pre_ping=True)
     State.cities = relationship("City",
-                                order_by=City.id, back_populates='states')
+                                order_by=City.id, back_populates='state')
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     data = session.query(City, State).\
-            filter(City.state_id == State.id).all()
+        filter(City.state_id == State.id).all()
     for x in data:
-        print("{}: ({}){}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(x[1].name, x[0].id, x[0].name))
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,12 +4,15 @@ and an instance Base = declarative_base() '''
 
 from model_state import Base, State
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 
 class City(Base):
-    ''' the class: state '''
+    ''' the class: city '''
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(states.id), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state = relationship("State", back_populates="cities")
