15d445ee61a06be64e514ff6d53eea103f3947c4
Tamir <hero2000.ts@gmail.com>
2024-08-06 08:26:23 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -19,4 +19,5 @@ if __name__ == "__main__":
     states = session.query(State).filter(State.name.like('%a%')).all()
     for state in states:
         session.delete(state)
+    session.commit()
     session.close()
