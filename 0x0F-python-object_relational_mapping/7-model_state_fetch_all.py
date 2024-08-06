a00d61476dba68249eb6deae53a9e23dd387c915
Tamir <hero2000.ts@gmail.com>
2024-08-06 06:44:05 -0700

    Another ALX Task

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa 
+"""lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -8,11 +8,15 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        states = session.query(State).all()
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-        session.close()
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
